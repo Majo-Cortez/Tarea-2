f76fcd20bb252d4eeb3f44c626a5d70cd15de241
Majo Cortez <52331693+Majo-Cortez@users.noreply.github.com>
2019-07-02 09:57:45 -0500

    Ejercicio modificado

--- Ejercicio.java
+++ Ejercicio.java
@@ -3,16 +3,19 @@
   public static void main(String[] args){
   float num,den;
   float num0,den0;
+  float num1,den1;
   Scanner lee=new Scanner(System.in);
   System.out.println("Ingrese el numerador");
   num=lee.nextFloat();
   System.out.println("Ingrese el denominador");
   den=lee.nextFloat();
-  Scanner lee=new Scanner(System.in);
   System.out.println("Ingrese el numerador0");
   num=lee.nextFloat();
   System.out.println("Ingrese el denominador0");
   den=lee.nextFloat();
-   
-}
+  System.out.println("Ingrese el numerador1");
+  num1=lee.nextFloat();
+  System.out.println("Ingrese el denominador1");
+  den1=lee.nextFloat(); 
+ }
 }
